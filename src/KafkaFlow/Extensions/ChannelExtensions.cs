26f7803dbd4b8b643008f69d4e49e2174192feed
Jack Tench <jack.tench@vonage.com>
2024-04-17 09:18:41 +0100

    Add a delay before reading messge from buffer Testing hacky fix for stalls when reading

--- src/KafkaFlow/Extensions/ChannelExtensions.cs
+++ src/KafkaFlow/Extensions/ChannelExtensions.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace KafkaFlow.Extensions;
 
@@ -11,7 +12,7 @@ internal static class ChannelExtensions
         this ChannelReader<T> reader,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        while (await reader.WaitToReadAsync(cancellationToken))
+        while (await WaitToReadAsync(reader, cancellationToken))
         {
             while (reader.TryRead(out var item))
             {
@@ -19,5 +20,11 @@ internal static class ChannelExtensions
                 yield return item;
             }
         }
+    }
+
+    private static async Task<bool> WaitToReadAsync<T>(ChannelReader<T> reader, CancellationToken cancellationToken)
+    {
+        await Task.Delay(10, cancellationToken);
+        return await reader.WaitToReadAsync(cancellationToken);
     }
 }
